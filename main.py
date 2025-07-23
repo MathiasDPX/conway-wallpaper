72ba0670ad34cbf79199b7f293fbbb14895877ab
MathiasDPX <mathias@dupeux.net>
2025-07-23 22:53:15 +0200

    make image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-import numpy as np
+from PIL import Image
 from engine import GameOfLife
 
 # Setup
@@ -20,3 +20,20 @@ game = GameOfLife()
 game.load("board.txt", canvas_size=canvas_size)
 game.step()
 game.save("board.txt")
+
+# Save image
+palette = config.get("palette", [[30,30,30], [0,122,204]])
+dead_cell_color = tuple(x for x in palette[0])
+live_cell_color = tuple(x for x in palette[1])
+
+img = Image.new("RGB", size=canvas_size, color=dead_cell_color)
+
+print(len(game.board), len(game.board[0]))
+
+for x in range(width-1):
+    for y in range(height-1):
+        if int(game.board[x][y]):
+            print("alive")
+            img.putpixel((x,y), live_cell_color)
+
+img.save("wallpaper.png")
